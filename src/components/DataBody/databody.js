4fea367951590b75a128ace8b67e4d044250d185
Gian Zamora <gz2016@att.net>
2021-06-24 08:33:48 -0700

    created a body insert module for user

--- src/components/DataBody/databody.js
+++ src/components/DataBody/databody.js
@@ -1,2 +1,29 @@
 import React, {useContext} from "react";
+import CreateContext from "../../utils/context";
 
+const setBody = () => {
+    let context = useContext(CreateContext);
+
+    return (
+        <section>
+            {context.developerState.filteredUsers.map( ({name, picture, email, dob }) => 
+                {
+                    return (
+                        <div data-th="image" className="align-middle">
+                            <img src={picture.medium} className='image-responsive'/>
+
+                            <h3 className='name-cell align-middle'>{name.last}, {name.first}</h3>
+                            
+                            <h5 className='align-middle'>{email}</h5>
+
+                            <h5 className='align-middle'>{dob.age}</h5>
+                        </div>
+                    )
+                }
+            )}
+
+        </section>
+    );
+}
+
+export default setBody;
