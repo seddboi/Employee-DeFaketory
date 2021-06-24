b553e4808a7f21b022b8fe036ced040feda06b5f
Gian Zamora <gz2016@att.net>
2021-06-23 23:34:50 -0700

    adjusted api call for 100 users

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export function getUsers() {
-    return axios.get("https://randomuser.me/api/")
+    return axios.get("https://randomuser.me/api/?results=100&nat=us")
 };
