55a38be19b4cc82eaea60f33de4b3c7910d963a3
Gian Zamora <gz2016@att.net>
2021-06-24 10:05:59 -0700

    added grouped data insert module component which gathers name informtion and body information for each user

--- src/components/DataInsert/datainsert.js
+++ src/components/DataInsert/datainsert.js
@@ -1,5 +1,25 @@
 import React, {useState, useEffect} from "react";
-import api from "../../utils/api.js";
+import setBody from "../components/DataBody/databody";
+import CreateContext from "../../utils/context";
 
-// import 
+const groupData = () => {
+    let context = useContext(CreateContext);
 
+    return (
+        <div className='datatable m-5'>
+            <table className='table table-condensed'>
+                {context.developerState.headings.map(({name}) => {
+                    return (
+                        <div onClick={ () => context.handleSort(name.toLowerCase())}>
+                            {name}
+                        </div>
+                    )
+                })}
+
+                <setBody />
+            </table>
+        </div>
+    );
+}
+
+export default groupData;
