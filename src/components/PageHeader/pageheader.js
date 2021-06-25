21a6f3eb08607ed3082750d2ceb2cb6e670da0c2
Gian Zamora <gz2016@att.net>
2021-06-24 20:00:44 -0700

    added header component

---
+++ src/components/PageHeader/pageheader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function insertHeader() {
+    
+    return (
+        <div className='header'>
+            <h2>Welcome to the Directory!</h2>
+            <hr/>
+            <h3>Use the search bar to find an employee or click the titles to filter!</h3>
+        </div>
+    );
+}
+
+export default insertHeader;
