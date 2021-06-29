8ae9487672c0b71099fb9c5c75b45a4497f65678
Gian Zamora <gz2016@att.net>
2021-06-28 18:37:00 -0700

    adjusted name of function variable and adjusted return value

--- src/components/DataBody/databody.js
+++ src/components/DataBody/databody.js
@@ -1,29 +1,27 @@
 import React, {useContext} from "react";
 import CreateContext from "../../utils/context";
 
-const SetBody = () => {
+const DataBody = () => {
     let context = useContext(CreateContext);
 
     return (
-        <section>
-            {context.developerState.filteredUsers.map( ({name, picture, email, dob }) => 
-                {
-                    return (
-                        <div data-th="image" className="align-middle">
-                            <img src={picture.medium} alt={`of ${name.first} ${name.last}`} className='image-responsive'/>
+        <section> 
+            {context.developerState.filteredUsers.map( ({name, picture, email, dob }) => {
+                return (
+                    <div data-th="image" className="align-middle">
+                        <img src={picture.medium} alt={`of ${name.first} ${name.last}`} className='image-responsive'/>
 
-                            <h3 className='name-cell align-middle'>{name.last}, {name.first}</h3>
+                        <h3 className='name-cell align-middle'>{name.last}, {name.first}</h3>
                             
-                            <h5 className='align-middle'>{email}</h5>
+                        <h5 className='align-middle'>{email}</h5>
 
-                            <h5 className='align-middle'>{dob.age}</h5>
-                        </div>
-                    )
-                }
-            )}
+                        <h5 className='align-middle'>{dob.age}</h5>
+                    </div>
+                );
 
+            })}
         </section>
     );
 }
 
-export default SetBody;
+export default DataBody;
